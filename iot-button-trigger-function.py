0f0132b3459a72475dee00eef23bdbbe8f4a2c71
David Malone <dmmal@amazon.com>
2019-01-23 10:52:34 -0500

    changed lambda function to use 1-click event format

--- iot-button-trigger-function.py
+++ iot-button-trigger-function.py
@@ -1,18 +1,70 @@
 '''
+This is a sample Lambda function that sends an SMS on click of a
+button. It needs one permission sns:Publish. The following policy
+allows SNS publish to SMS but not topics or endpoints.
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "sns:Publish"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Deny",
+            "Action": [
+                "sns:Publish"
+            ],
+            "Resource": [
+                "arn:aws:sns:*:*:*"
+            ]
+        }
+    ]
+}
+SMS messaging via SNS is only available in the following AWS IoT 1-Click regions:
+- US East (N. Virginia)
+- US West (Oregon)
+- EU (Ireland)
+- Asia Pacific (Tokyo)
 The following JSON template shows what is sent as the payload:
 {
-    "serialNumber": "GXXXXXXXXXXXXXXXXX",
-    "batteryVoltage": "xxmV",
-    "clickType": "SINGLE" | "DOUBLE" | "LONG"
+    "deviceInfo": {
+        "deviceId": "GXXXXXXXXXXXXXXX",
+        "type": "button",
+        "remainingLife": 98.7,
+        "attributes": {
+            "projectName": "Sample-Project",
+            "projectRegion": "us-west-2",
+            "placementName": "Room-1",
+            "deviceTemplateName": "lightButton"
+        }
+    },
+    "deviceEvent": {
+        "buttonClicked": {
+            "clickType": "SINGLE",
+            "reportedTime": 1521159287205
+        }
+    },
+    "placementInfo": {
+        "projectName": "Sample-Project",
+        "placementName": "Room-1",
+        "attributes": {
+            "key1": "value1"
+        },
+        "devices": {
+            "lightButton":"GXXXXXXXXXXXXXXX"
+        }
+    }
 }
-
 A "LONG" clickType is sent if the first press lasts longer than 1.5 seconds.
 "SINGLE" and "DOUBLE" clickType payloads are sent for short clicks.
-
 For more documentation, follow the link below.
 http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
 '''
-
 from __future__ import print_function
 import boto3
 import json
@@ -21,18 +73,19 @@ import pprint
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
 sns = boto3.client('sns')
 iotDataClient = boto3.client('iot-data')
-phone_number = '15551230987'  # change it to your phone number
 
+thingName = 'etogw4_thing'
 
 def lambda_handler(event, context):
     logger.info('Received event: ' + json.dumps(event))
 
-    if event["clickType"] == "SINGLE":
+    click_type = event['deviceEvent']['buttonClicked']['clickType']
+
+    if click_type == "SINGLE":
       ledState = "on"
-    elif event["clickType"] == "DOUBLE":
+    elif click_type == "DOUBLE":
       ledState = "off"
 
     payload = {
@@ -43,12 +96,20 @@ def lambda_handler(event, context):
       }
     }
 
-    result = iotDataClient.update_thing_shadow(thingName='rpi-sense-hat', payload=json.dumps(payload))
+    result = iotDataClient.update_thing_shadow(thingName=thingName, payload=json.dumps(payload))
     print('update thing shadow result:')
     pp = pprint.PrettyPrinter()
     pp.pprint(json.loads(result['payload'].read()))
 
 
-    message = 'Hello from your IoT Button %s. Here is the full event: %s' % (event['serialNumber'], json.dumps(event))
+    attributes = event['placementInfo']['attributes']
+    phone_number = attributes['phoneNumber']
+    message = attributes['message']
+    for key in attributes.keys():
+        message = message.replace('{{%s}}' % (key), attributes[key])
+    message = message.replace('{{*}}', json.dumps(attributes))
+    dsn = event['deviceInfo']['deviceId']
+
+    message += '\n(DSN: {}, {})'.format(dsn, click_type)
     sns.publish(PhoneNumber=phone_number, Message=message)
     logger.info('SMS has been sent to ' + phone_number)
